000f17f9c4f509b86abfe2e89b7f9aefc4fcf988
Andersonmpereira <67236179+Andersonmpereira@users.noreply.github.com>
2020-06-22 08:21:56 -0300

    Update Concluido.php

--- Concluido.php
+++ Concluido.php
@@ -10,13 +10,19 @@
     <body id="conclusao">         
         <div class="container larg-600">
             <div class="row">
-                <img class="element-center" src="http://reform.areiaquecanta.com.br/wp-content/uploads/2020/05/logo.png" alt="Areiaquecanta" width="200">  
+                <img class="element-center" src="" alt="" width="200">  
             </div>
             <div class="row">
                 <h1 class="element-center txt-white text-center text-pad">Obrigado!</h1>
-                <p class="element-center txt-white text-center text-pad">Obrigado por colaborar com sua opinião.<br>
-                Aproveitando o contato, gostaríamos de apresentar um novo produto para você, que já nos conhece: <strong>O Créditos de Viagem Areia Que Canta.</strong>  <br>Preparamos uma página com todas as informações, disponível no botão abaixo. Acreditamos que você pode se interessar. Qualquer dúvida, por favor contate nossa equipe de recepcionistas.</p>
-                <a class="btn btn-white element-center" type="button" href="https://areiaquecanta.com.br/creditos-de-viagem#pacotes" value="">Créditos de Viagem</a>
+                <p class="element-center txt-white text-center text-pad">
+                    Obrigado por colaborar com sua opinião.
+                    <br>
+                    Aproveitando o contato, gostaríamos de apresentar um novo produto para você, que já nos conhece: 
+                    <strong>O Créditos de Viagem.</strong>  
+                    <br>
+                    Preparamos uma página com todas as informações, disponível no botão abaixo. Acreditamos que você pode se interessar. Qualquer dúvida, por favor contate nossa equipe de recepcionistas.
+                </p>
+                <a class="btn btn-white element-center" type="button" href="" value="">Créditos de Viagem</a>
                 <br>
                 <br>
             </div>
